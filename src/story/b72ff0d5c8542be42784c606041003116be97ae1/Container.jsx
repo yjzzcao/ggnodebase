8f1f8416448b93589efdeadaf7aa506f3e2df8cc
yjzzcao <yjzzcao@gmail.com>
2018-08-05 11:05:47 +0800

    添加故事雪花页面

---
+++ src/story/b72ff0d5c8542be42784c606041003116be97ae1/Container.jsx
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+
+class Container extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="banner">
+                    <img src="TODO" alt="TODO" />
+                    <div className="note">TODO</div>
+                </div>
+                <div className="container">
+                    <img className="cut-off-line" src={require('./images/故事详情分割线.png')} alt="故事详情分割线" />
+                    <div className="box">
+                        <ol className="breadcrumb">
+                            <li>首页</li>
+                            <li>成功故事</li>
+                            <li className="active">TODO</li>
+                        </ol>
+                        <h3>TODO</h3>
+                        <p>TODO</p>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default Container;
