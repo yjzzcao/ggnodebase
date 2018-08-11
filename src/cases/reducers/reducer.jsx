63019f74d43692f88d6526a0045b74dbb38c829a
yjzzcao <yjzzcao@gmail.com>
2018-08-12 00:51:55 +0800

    完成底部链接添加

--- src/cases/reducers/reducer.jsx
+++
@@ -1,7 +0,0 @@
-import ID from '../ID';
-import DB from 'Common/GetData/DB';
-
-const reducer = {};
-reducer[ID] = DB(ID);
-
-export default reducer;
--- src/cases/reducers/store.jsx
+++
@@ -1,6 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import reducer from './reducer';
-
-const store = createStore(combineReducers(reducer));
-
-export default store;
