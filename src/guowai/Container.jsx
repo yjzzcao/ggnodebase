1d9e9110f1232a2de3ecdeb636c28b548c7c06e6
yjzzcao <yjzzcao@gmail.com>
2018-08-11 14:00:21 +0800

    完成海外页面

--- src/guowai/Container.jsx
+++ src/guowai/Container.jsx
@@ -1,7 +1,219 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => {
-    return (
-        <div className="container">Welcome Express React</div>
-    );
-};
+class Container extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="left">
+                    <div id="navbar" className="sidebar">
+                        <ul className="nav nav-pills nav-stacked" data-spy="affix" data-offset-top="78">
+                            <li className="active"><a href="#basic">平台概况</a></li>
+                            <li><a href="#product">平台产品</a></li>
+                            <li><a href="#service">平台服务</a></li>
+                            <li><a href="#super">平台优势</a></li>
+                            <li><a href="#case">成功案例</a></li>
+                            <li><a href="#introduce">客户介绍</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="right">
+                    <section id="basic">
+                        <div className="title">平台概况</div>
+                        <div className="content">
+                            <p>
+                                梯电传媒的海外营销服务品牌名称为“出海易”。出海易作为全球媒体资源平台，为出海企业提供落地营销解决方案，让企业全球化更加容易。<br/>
+                                出海易是向中国企业提供海外落地营销解决方案的领军品牌，作为全球优质媒体资源平台，公司帮助中国两万家出海企业在全球快速扩张，让企业享受“一带一路”的巨大历史机遇，坐拥全球化红利，赢得企业的国际声誉与收益。
+                            </p>
+                            <p>
+                                出海易在全球掌握城市电梯媒体核心资源，电梯作为一个国家与一座城市的重要基础设施，是最日常的生活场景，电梯更是城市主流人群每天的必经之路，具有高频与低干扰的特点，这正是在今天能够引爆品牌的最核心以及最稀缺的资源。出海易的业务已经覆盖主要的亚洲国家，包括印度、韩国、巴基斯坦、孟加拉国、泰国、菲律宾、印尼、越南、马来西亚、新加坡等10余个国家，而且正在逐步拓展欧美的业务。每天出海易的媒体资源覆盖4亿城市人口，超过200多个城市以及200万个电梯终端。
+                            </p>
+                            <p>
+                                出海易的客户主要针对中国有出海需求的企业，中国企业出海是大势所趋，许多中国品牌都在积极贯彻2013年习近平主席提出的“一带一路”倡议，响应国内新时代全球化号召，这也成为了有史以来国内品牌进军海外的最大发展动力。这些品牌包括联想、华为、小米、OPPO、VIVO、一加等消费电子行业、也包括阿里巴巴、腾讯、百度、京东、猎豹等互联网企业、还包括海尔、海信、TCL、格力、美的等家电企业，更有吉利、长城、比亚迪、奇瑞、江淮等汽车品牌。
+                            </p>
+                        </div>
+                    </section>
+                    <section id="product">
+                        <div className="title">平台产品</div>
+                        <div className="content">
+                            <p>
+                                <span className="blank">电梯海报资源</span><br/>
+                                覆盖城市主要的社区公寓，电梯内环境干扰度极低，目标人群平均每周上下超20次，每周收视时间超10分钟，电梯海报在必经的封闭狭小电梯空间内形成强制性收视，在乘坐的无聊时间形成极高的广告关注度，相较于其他媒体，受众人群会更加仔细用心的去看电梯海报上的广告，而且当一家人做电梯时还会经常讨论电梯内的广告，更有很多父母带着孩子上下电梯时会向孩子介绍广告内容，这都形成非常强大的品牌互动与传播。
+                            </p>
+                            <p>
+                                <span className="blank">电梯电视资源</span><br/>
+                                覆盖城市主要的中高端写字楼，80%的受众年龄在20-50岁之间，主要为职场人士，他们是一个国家的中流砥柱，他们是社会财富的主要创造者，有着巨大的消费潜力，目标人群平均每日接触电梯电视3次，平均每次接触时长约2分钟。电梯电视在受众等待电梯以及电梯内的有限时间里形成强制内容输出，是抢占中产人群消费认知的必要媒体，可以有效的提升品牌势能。
+                            </p>
+                        </div>
+                    </section>
+                    <section id="service">
+                        <div className="title">平台服务</div>
+                        <div className="content">
+                            <p>
+                                出海易向出海客户提供专业的市场营销服务，根据客户的市场策略，提供针对性的解决方案。<br/>
+                                目前出海企业主要面临三个困难：
+                            </p>
+                            <p>
+                                <span className="blank">1. 知名度低阻碍品牌在全球范围内被接受</span><br/>
+                                对于想要“走出去”的品牌，这是需要克服的首要障碍，因此在目标群体中提高品牌知名度仍是重中之重。出海易熟悉海外市场，通过专业的品牌市场调研，向客户提供本土化的品牌策略建议，并采用实际的市场数据进行论证说明。<br/>
+                                <span className="blank">2. 仍需改善信任感</span><br/>
+                                许多年轻消费者认为中国品牌善于创新，但对质量仍存有疑虑。部分中国品牌因制造优质产品而享有良好信誉，但仍需以更大力度的宣传来改变消费者对中国品牌的固有印象。出海易在宣传上不仅有核心的电梯媒体资源，更有一定的渠道资源，能够更好的让企业品牌、业务渠道与客户关系进行融合，以改善消费者的信任度。<br/>
+                                <span className="blank">3. 缺乏品牌建设投资</span><br/>
+                                中国品牌应该更加认识到营销投资是赢得成功的关键，并且这些投资决策需要以了解本地市场和消费者为基础。品牌应加倍专注最具实效的媒体渠道并且开展更强大、更具影响力的广告宣传。出海易掌握的媒体资源将帮助品牌引爆主流人群，并进行充分的品牌互动与口碑传播。
+                            </p>
+                        </div>
+                    </section>
+                    <section id="super">
+                        <div className="title">平台优势</div>
+                        <div className="content">
+                            <p>出海易的媒体资源优势主要体现在四个方面：</p>
+                            <p>
+                                <span className="blank">1. 知投放精准</span><br/>
+                                未来可通过人脸识别精准覆盖城市主流消费人群<br/>
+                                <span className="blank">2. 投放灵活</span><br/>
+                                可根据广告主的需求精准覆盖特定区域<br/>
+                                <span className="blank">3. 表现力强</span><br/>
+                                影像清晰度高，画面尺寸大；声画统一，冲击力强<br/>
+                                <span className="blank">4. 重复影响</span><br/>
+                                高频率的播放频次，不断加深受众的品牌印象
+                            </p>
+                            <p>
+                                这些优势可以为企业提供一条快速实效的传播途径。以楼宇媒体为载体，有效提高目标人群的接受度和企业的公信度，增强品牌的竞争力。同时，随着线上流量成本的升高和线上流量的碎片分散化，线下流量必然是品牌商的必争之地。现代都市人的品牌消费习惯不是一朝一夕养成的，电梯空间作为品牌潜移默化推进的阵地之一，是把握线下流量和引爆品牌的重要渠道。
+                            </p>
+                            <p>
+                                出海易掌握的电梯媒体资源引导城市主流人群的消费，覆盖城市主流人群的工作场景与生活场景，在信息多元化、碎片化的大环境下，出海易的媒体资源是消费者必经的、封闭的、视野范围狭窄、低干扰的线下生活圈场景，这些特征是电视、杂志、户外等传统媒体难以实现的。<br/>
+                                根据调查，我们可以看到：
+                            </p>
+                            <p><img src={require('./images/海外优势图表.png')} alt="海外优势图表" /></p>
+                            <p>
+                                从各媒介的刊例变化看，电梯电视、电梯海报、影视视频与互联网四种媒介呈现大幅增长；从广告到达率看，互联网广告、电视广告和电梯广告位居前三大，其中电梯广告到达率和关注度成正比，比例均较高，电梯广告的周到达率为74%；<br/>
+                                从全媒体广告眼球份额看，城市消费者每天观看广告时间为24分钟，其中互联网占6.2分钟比重为26%，电视广告占5分钟比重为21%，电梯广告占4.6分钟比重为19%；<br/>
+                                从平面广告每天的接触点看，电梯海报覆盖一天的早中晚高峰时间，互联网则覆盖一天的其余时间。
+                            </p>
+                            <p>
+                                此外，付费去广告，主动规避广告成为趋势，所以移动互联网广告打造品牌的效果就会被大幅稀释。也就是说，主流人群虽然都在看互联网，但人们在互联网上很少留意广告。而电梯广告属于公共媒体，出现在消费闲暇等候时段，被视作信息提供源，抗拒感较弱，广告接受度较高，而且电梯广告的受众质量高，受众年龄分布均匀，核心消费群体年龄集中在25-50岁，收入也相对较高，也就是说电梯媒体受众人群更优质。
+                            </p>
+                            <p>
+                                综合来看，在传统媒体全面下跌的背景下，电梯媒体的持续上升意味着城市主流人群的基础场景媒体的价值日益凸显，而且城市人口越发集聚，出海易的电梯媒体资源价值更具有优势。
+                            </p>
+                        </div>
+                    </section>
+                    <section id="case">
+                        <div className="title">成功案例</div>
+                        <div className="content">
+                            <li className="item clearfix">
+                                <img src={require('./images/全部成功故事4.png')} alt="全部成功故事4"/>
+                                <div className="content">
+                                    <div className="title">著名汽车品牌成功出海越南</div>
+                                    <div className="note">对于中国汽车品牌想要在越南与日系、德系、韩系汽车争得市场份额并不容易，先如今，该汽车品牌已经在越南建立了组装厂，并在当地实现了良好的运营与销售，而这一切良好的开端都是好的产品与营销策略的完美结合...</div>
+                                    <a href="javascript:void(0)" className="more">点击查看 ></a>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/全部成功故事5.png')} alt="全部成功故事5"/>
+                                <div className="content">
+                                    <div className="title">知名手机品牌成功出海印度</div>
+                                    <div className="note">开疆拓土并非易事，而进军印度市场更是难上加难，这是非常棘手的市场，但不管面临多么艰巨的困难，勇往直前就能够扭转局面。对于该品牌在印度的成功，主要做对了三个事情...</div>
+                                    <a href="javascript:void(0)" className="more">点击查看 ></a>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/全部成功故事6.png')} alt="全部成功故事6"/>
+                                <div className="content">
+                                    <div className="title">新兴互联网品牌成功出海马来西亚</div>
+                                    <div className="note">该品牌此前主要通过搜索引擎、展示广告、联盟广告、邮件营销等方式进行推广，其中，搜索引擎是第一大流量入口，其有超过一半的流量来自谷歌，但是该企业逐渐发现与高额互联网营销费用不相匹配的是客户粘度很低...</div>
+                                    <a href="javascript:void(0)" className="more">点击查看 ></a>
+                                </div>
+                            </li>
+                        </div>
+                    </section>
+                    <section id="introduce">
+                        <div className="title">客户介绍</div>
+                        <div className="content">
+                            <li className="item clearfix">
+                                <img src={require('./images/海外客户1.png')} alt="海外客户1"/>
+                                <div className="content">
+                                    <div className="title">联想</div>
+                                    <div className="note">作为个人电脑和移动技术的全球领军企业，联想的产品包括个人电脑、智能手机、平板电脑、智能电视等。其重大的跨国收购活动包括2005年收购IBM的个人电脑业务，以及从Google收购摩托罗拉移动公司。联想在中国之外的移动销售业务最近几年持续增长，其产品在全球160多个国家均有销售。</div>
+                                </div>
+                            </li>
+                            <li className="item obb clearfix">
+                                <img src={require('./images/海外客户2.png')} alt="海外客户2"/>
+                                <div className="content">
+                                    <div className="title">华为</div>
+                                    <div className="note">作为个人电脑和移动技术的全球领军企业，联想的产品包括个人电脑、智能手机、平板电脑、智能电视等。其重大的跨国收购活动包括2005年收购IBM的个人电脑业务，以及从Google收购摩托罗拉移动公司。联想在中国之外的移动销售业务最近几年持续增长，其产品在全球160多个国家均有销售。</div>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/海外客户3.png')} alt="海外客户3"/>
+                                <div className="content">
+                                    <div className="title">阿里巴巴</div>
+                                    <div className="note">阿里巴巴是中国领先的电子商务在线交易平台。在2017财年，阿里巴巴集团的收益为1582.7亿元人民币（与去年相比增长了56%）。尽管海外收益目前只占其中很小的一部分，海外业务扩张作为公司的长期战略，相应收益未来预期会有高速的增长。公司的海外平台全球速卖通(AliExpress)为全球消费者带来中国的产品，而天猫国际则将国际商品带给中国消费者。</div>
+                                </div>
+                            </li>
+                            <li className="item obb clearfix">
+                                <img src={require('./images/海外客户4.png')} alt="海外客户4"/>
+                                <div className="content">
+                                    <div className="title">小米</div>
+                                    <div className="note">小米是一家经营智能手机及其他移动设备的制造商，也生产各种家电及消费电子产品和配件。2014年，小米开始其出口业务，同年成为了全球最具价值的新兴科技企业。其主要海外市场包括印度、乌克兰、印度尼西亚、缅甸和以色列等。小米计划在未来大举进军欧洲市场，并将目光首先投向了东欧。</div>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/海外客户5.png')} alt="海外客户5"/>
+                                <div className="content">
+                                    <div className="title">海尔</div>
+                                    <div className="note">海尔的空调、洗衣机和冰箱在全球100多个国家均有销售。超过一半的海尔产品都在其位于中国之外的工厂生产制造。海外销售对其年度收益的贡献超过四分之一。2016年4月，海尔收购了通用电气的电器业务，大大提升了在美国市场的份额。公司不断创造新的产品满足全球消费者的需求。</div>
+                                </div>
+                            </li>
+                            <li className="item obb clearfix">
+                                <img src={require('./images/海外客户6.png')} alt="海外客户6"/>
+                                <div className="content">
+                                    <div className="title">一加</div>
+                                    <div className="note">一加是物美价廉的智能手机品牌，2014年开始在中国和世界市场发售，销往全球40多个国家。美国、欧洲、印度是公司销售额的主要来源。一加手机主要通过线上渠道销售，并且在印度也开设了快闪店和服务中心。</div>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/海外客户7.png')} alt="海外客户7"/>
+                                <div className="content">
+                                    <div className="title">海信</div>
+                                    <div className="note">海信旗下的移动设备和家用电器、电视等在全球80多个国家均有销售。从2016年1月到9月，海信在欧洲的销量增长了 36.8%；从2017年1月到5月，海信电视的出口比去年同期增 长了28.2%。此外，除了赞助2016年欧洲杯，海信还是俄罗斯2018年足球世界杯和红牛F1车队的全球赞助商。</div>
+                                </div>
+                            </li>
+                            <li className="item obb clearfix">
+                                <img src={require('./images/海外客户8.png')} alt="海外客户8"/>
+                                <div className="content">
+                                    <div className="title">TCL</div>
+                                    <div className="note">TCL集团专攻多媒体娱乐和智能电视设备，同时也生产 DVD播放器、空调、冰箱和其他大型家电。集团生产的移动设备以其十年前收购的Alcatel品牌进行销售。TCL集团在全球80多个国家均有营业部，销售面覆盖全球160个国家。公司的业务收入部分有一半来自海外市场。</div>
+                                </div>
+                            </li>
+                            <li className="item clearfix">
+                                <img src={require('./images/海外客户9.png')} alt="海外客户9"/>
+                                <div className="content">
+                                    <div className="title">OPPO</div>
+                                    <div className="note">OPPO设计、生产、销售智能手机和包括蓝光播放器在内的消费电子产品。公司成立之初以生产MP3和MP4为主，四年后开始生产移动设备。OPPO在2009年末开始拓展海外市场，泰国是它的第一个目的地。海外市场占OPPO总销售额的20%，目前，印度和东南亚是主要收入来源。</div>
+                                </div>
+                            </li>
+                            <li className="item obb clearfix">
+                                <img src={require('./images/海外客户10.png')} alt="海外客户10"/>
+                                <div className="content">
+                                    <div className="title">VIVO</div>
+                                    <div className="note">VIVO生产和销售智能手机。2014年起，VIVO开始进入海外市场，逐渐在印度和其他东南亚国家站稳脚跟。VIVO智能手机的出货量已经位列世界第五，占据5.8%的全球市场份额。VIVO还是2018和2022年足球世界杯的官方赞助商。现VIVO加快了进军海外市场的步伐，业务扩展到俄罗斯以及非洲地区。</div>
+                                </div>
+                            </li>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        );
+    }
+    componentDidMount() {
+        $(window).on('hashchange', function () {
+            const $target = $(location.hash);
+            if ($target.length === 1) {
+                const top = $target.offset().top - 78;
+                $('html,body').animate({ scrollTop: top }, 200);
+            }
+        });
+    }
+}
+
+export default Container;
