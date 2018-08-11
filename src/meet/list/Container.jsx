ed8c03ca9edf12cecadfb5878f59677361bcf787
yjzzcao <yjzzcao@gmail.com>
2018-08-12 01:08:19 +0800

    会议页面链接链接完成

--- src/meet/list/Container.jsx
+++ src/meet/list/Container.jsx
@@ -10,7 +10,7 @@ class Container extends Component {
                 </ol>
                 <h1>会议活动</h1>
                 <div className="list">
-                    <a href="javascript:void(0)" className="item">
+                    <a href="/meet/1a9828ea959731d2674b3734afcae6609d97cacd" className="item">
                         <img src={require('./images/全部会议内容1.png')} alt="全部会议内容1" />
                         <div className="content">
                             <div className="create-time">会议活动 | 2018年9月5-9月7日</div>
@@ -18,7 +18,7 @@ class Container extends Component {
                             <div className="note">中国 上海</div>
                         </div>
                     </a>
-                    <a href="javascript:void(0)" className="item">
+                    <a href="/meet/97d750cd5315e93a9c26895c1a6df27df828b1c4" className="item">
                         <img src={require('./images/全部会议内容2.png')} alt="全部会议内容2" />
                         <div className="content">
                             <div className="create-time">会议活动 | 2018年9月3日-9月4日</div>
@@ -26,7 +26,7 @@ class Container extends Component {
                             <div className="note">中国 北京</div>
                         </div>
                     </a>
-                    <a href="javascript:void(0)" className="item">
+                    <a href="/meet/cbe1b78f88c80eb22d0878b0e792d8b4403179ba" className="item">
                         <img src={require('./images/全部会议内容3.png')} alt="全部会议内容3" />
                         <div className="content">
                             <div className="create-time">会议活动 | 2018年6月21日</div>
@@ -34,7 +34,7 @@ class Container extends Component {
                             <div className="note">中国 北京</div>
                         </div>
                     </a>
-                    <a href="javascript:void(0)" className="item">
+                    <a href="/meet/b06d9557696d0018507c980c47ee6dce161bab65" className="item">
                         <img src={require('./images/全部会议内容4.png')} alt="全部会议内容4" />
                         <div className="content">
                             <div className="create-time">会议活动 | 2018年6月13日</div>
@@ -42,7 +42,7 @@ class Container extends Component {
                             <div className="note">中国 上海</div>
                         </div>
                     </a>
-                    <a href="javascript:void(0)" className="item">
+                    <a href="/meet/048df0fe5b8b66f24ab60afc557f4374602a83f5" className="item">
                         <img src={require('./images/全部会议内容5.png')} alt="全部会议内容5" />
                         <div className="content">
                             <div className="create-time">会议活动 | 2018年5月4日 </div>
