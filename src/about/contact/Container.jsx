48375244433f5add6dff403f73775fc02855e858
yjzzcao <yjzzcao@gmail.com>
2018-08-12 16:38:21 +0800

    文字修改

--- src/about/contact/Container.jsx
+++ src/about/contact/Container.jsx
@@ -8,10 +8,10 @@ class Container extends Component {
                     <div className="navbar">
                         <div className="title">关于电梯</div>
                         <ul className="nav nav-pills nav-stacked">
-                        <li><a href="/about/abstract">公司简介</a></li>
+                            <li><a href="/about/abstract">公司简介</a></li>
                             <li><a href="/about/manage">公司治理</a></li>
-                            <li><a href="/about/join">电梯招聘</a></li>
-                            <li><a href="/about/contact">联系电梯</a></li>
+                            <li><a href="/about/join">梯电招聘</a></li>
+                            <li><a href="/about/contact">联系梯电</a></li>
                         </ul>
                     </div>
                 </div>
--- src/about/join/Container.jsx
+++ src/about/join/Container.jsx
@@ -10,8 +10,8 @@ class Container extends Component {
                         <ul className="nav nav-pills nav-stacked">
                             <li><a href="/about/abstract">公司简介</a></li>
                             <li><a href="/about/manage">公司治理</a></li>
-                            <li><a href="/about/join">电梯招聘</a></li>
-                            <li><a href="/about/contact">联系电梯</a></li>
+                            <li><a href="/about/join">梯电招聘</a></li>
+                            <li><a href="/about/contact">联系梯电</a></li>
                         </ul>
                     </div>
                 </div>
